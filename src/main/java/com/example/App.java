f807e7a6ef07c95d0cc869976b9d85bcc3dae822
Ivan Hermawan <ivanhermawan19@gmail.com>
2022-09-29 10:30:38 +0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(App.class);
-        log.info("Java app started Test welcome");
+        log.info("Java app started Test welcome welcome");
     }
 
     public String getStatus() {
